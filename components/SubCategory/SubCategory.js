27a212236d6b16cfa81e79e4be9ef2643329d195
vigneshdharuman01 <vigneshdharuman01@gmail.com>
2024-01-27 13:02:01 +0530

    subcategory page responsive complited

--- components/SubCategory/SubCategory.js
+++ components/SubCategory/SubCategory.js
@@ -11,6 +11,8 @@ import Loader from '../Sub-components/Loader'
 import { getButtonShown } from '../../Slice/ProductDetailsSlice'
 import { getProductDetails } from '../../Slice/ProductDetailsSlice'
 import BottomTab from "../Sub-components/ButtomTab/BottomTab";
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+
 
 const SubCategory = ({navigation}) => {
     const dispatch=useDispatch();
@@ -23,9 +25,9 @@ const SubCategory = ({navigation}) => {
 
     const encodedcategoryname = encodeURIComponent(useSelector((state)=>state.category.categoryname));
     const encodedcategoryid = encodeURIComponent(useSelector((state)=>state.category.categoryid));
-    
-    // console.error(useSelector((state)=>state.category.categoryid))
-    useEffect(()=>{
+
+       // console.error(useSelector((state)=>state.category.categoryid))
+       useEffect(()=>{
        
         //  const subcategoryapi='https://www.texasknife.com/dynamic/texasknifeapi.php?action=cus_sub_category&category_id=2' 
       
@@ -86,44 +88,34 @@ const SubCategory = ({navigation}) => {
         navigation.navigate('Profile')
     }
     
-
   return (
-   <View style={styles.subcategory}>
-   <View style={styles.subcategory_container}>
-    <View style={styles.subcategory_header_conatiner}>
-    <TouchableOpacity style={{paddingRight:10,padding:5}} onPress={()=>navigation.navigate('Home')}>
-        <Icon name="arrow-left" size={25} color="#2f2e7e" style={{marginLeft:10}} onPress={()=>navigation.navigate('Home')}/>
-        </TouchableOpacity>
    
-        <Text  style={styles.subcategory_header}>{useSelector((state)=>state.category.categoryname)}</Text>
-<View></View>
+     <View style={styles.Subcategory}>
 
-        {/* <TouchableOpacity style={{paddingLeft:10,padding:5}}  onPress={()=>navigation.navigate('cart')}>
-         
-        <Icon name="shopping-cart" size={25} color="#2f2e7e" style={{marginRight:10,position:'relative'}} onPress={()=>navigation.navigate('cart')}/>  
-        <View style={styles.cart_icon_text_container}>
-        <Text style={styles.cart_text}>12</Text>
+        <View style={styles.header}>
+          <View style={styles.header_container}>
+        <Text  style={styles.header_text}>{useSelector((state)=>state.category.categoryname)}</Text>
+          </View>
         </View>
-        </TouchableOpacity> */}
-       
-    </View>
-{
-    loader?(
-<View>
-    { subCategoryData.status===200 ? (
-           <View style={styles.subcategory_list_container}>
+
+        <View style={styles.content}>
+          {loader?(
+            <View style={{flex:1}}>
+                {subCategoryData.status===200 ? (
+                <View style={styles. subcategory_list_container} >
  {
+    
             <FlatList
             data={subCategoryData.data}
             keyExtractor={i=>i.id}
             key={'_'}
-              numColumns={2}
+             numColumns={2}
             renderItem={({item})=>{
              return(
                 <TouchableOpacity style={styles.subcategory_list_touchable_opacity}  key={item.id} onPress={()=>calldata(item)}>
                 <View style={styles.subcategory_list}>
                    <View style={styles.subcategory_image_container}>
-                   <Image style={{width:"100%",height:"100%",resizeMode:'contain'}}  source={{ uri: item.image }} />
+                   <Image style={{width:"100%",height:"100%",resizeMode:'contain',}}  source={{ uri: item.image }} />
                    </View>
                    <View style={styles.subcategory_name_container}>
                        <Text style={styles.subcategory_name}>{item.name}</Text>
@@ -135,197 +127,116 @@ const SubCategory = ({navigation}) => {
             }}
             />
         }
+                </View>):(
+                      <View style={styles.nocategorycontainer}>
+                      <FlatList
+                          data={nocategorydata}
+                          keyExtractor={i=>i.id}
+                          
+                          renderItem={({item})=>{
+                           return(
+                          
+                            <TouchableOpacity keyExtractor key={item.id} onPress={()=>handleChange(item)} >
+                            <View style={styles.list}>
+                               <View style={styles.image_container}>
+                                <Image style={{width:'100%',height:'100%',resizeMode:'stretch'}}  source={{ uri: item.product_image }}/>
+                               </View>
+                               <View style={styles.name_price_container}>
+                                <Text style={styles.item_name}>{item.product_name}</Text>
+                                <Text style={styles.item_price}>{item.product_price}</Text>
+                               </View>
+                            </View>
+                            </TouchableOpacity> 
+                           )
+                          }}
+                          />
+                      </View>
+                  )}
+            </View>
+          ):(
+             <Loader/>
+          )}
         </View>
-    ):(
-        <View style={styles.nocategorycontainer}>
-        <FlatList
-            data={nocategorydata}
-            keyExtractor={i=>i.id}
-            
-            renderItem={({item})=>{
-             return(
-            
-              <TouchableOpacity keyExtractor key={item.id} onPress={()=>handleChange(item)} >
-              <View style={styles.list}>
-                 <View style={styles.image_container}>
-                  <Image style={{width:'100%',height:'100%',resizeMode:'stretch'}}  source={{ uri: item.product_image }}/>
-                 </View>
-                 <View style={styles.name_price_container}>
-                  <Text style={styles.item_name}>{item.product_name}</Text>
-                  <Text style={styles.item_price}>{item.product_price}</Text>
-                 </View>
-              </View>
-              </TouchableOpacity> 
-             )
-            }}
-            />
+        <View style={styles.footer}>
+          <BottomTab/>
         </View>
-    )}
-    </View>
-    ):(
-        <Loader/>
-    )
-}
+     </View>
 
-   
-
-
-  
-   </View>
-   <BottomTab home={home} cart={cart} profile={profile} />
-
-   </View>
   )
 }
-const styles=StyleSheet.create({
-    subcategory:{
-        ...Platform.select({
-            android:{
-                width: "100%",
-                height: "93%"
-            },
-            ios:{
-                width: "100%",
-                height: "93%"
-            },
-          })
-    },
-    subcategory_container:{
-        // backgroundColor:'red',
-        width:'100%',
-        height:'100%'
-    },
-    subcategory_header_conatiner:{
-        // backgroundColor:'yellow',
-        marginTop:30,
-        padding:10,
-        flexDirection:'row',
-        justifyContent:'space-between',
-        alignItems:'center',
-    },
-        subcategory_header:{
-            fontSize:16,
-            fontWeight:'bold',
-            color:'#2a2e7e',
-            // backgroundColor:'red',
-            
-        },
-    subcategory_list_container:{
-        width:"100%",
-        height:700,
-        // backgroundColor:'red',
-         alignItems:'center',
-        justifyContent:'space-evenly',
-     
-    },
-    subcategory_list_touchable_opacity:{
-        borderWidth:2,
-        width:150,
-        height:200,
-        borderColor:'#2f2e7e',
-        margin:10,
-        // backgroundColor:'yellow'
-       
-    },
-    subcategory_list:{
-         alignItems:'center',
-    },
-    subcategory_image_container:{
-        width:"100%",
-        height:120,
-        // backgroundColor:"orange",
-        padding:10  
-    },
-    subcategory_name_container:{
-        // backgroundColor:'yellow',
-        width:'100%',
-        height:70,
-        alignItems:'center',
-        justifyContent:'center'
-    },
-    subcategory_name:{
-        textAlign:'center',
-        color:'#2f2e7e',
-        fontWeight:'500',
-        padding:5
-    },
-    cart_icon_text_container:{
-        position:'absolute',
-        // backgroundColor:'white',
-        width:15,
-        height:15,
-        borderRadius:50,
-        left:16,
-         top:7,
-        justifyContent:'center',
-        alignItems:'center'
-    },
-    cart_text:{
-    color:'#ffffff',
-    fontWeight:'bold',
-    fontSize:8,
-    padding:2
-    },  
 
-    // nocategory area
+const styles = StyleSheet.create({
+Subcategory:{
+flex:1,
+backgroundColor:'#ffffff'
+},
+header:{
+// backgroundColor:'yellow',
+flex:1,
+justifyContent:'center',
+alignItems:'center',
+},
+content:{
+// backgroundColor:'pink',
+flex:8
+},
+footer:{
+backgroundColor:'white',
+flex:0.7,
+justifyContent:'flex-end'
+},
+header_container:{
+    flex:1,
+    // height:hp('30%'),
+    justifyContent:'center',
+    alignItems:'center',
+    // backgroundColor:'red',
+    paddingTop:hp(2)
+},
+header_text:{
+    fontSize:hp(2.1),
+    color:'#2a2e7e'
+},
+subcategory_list_container:{
+    // backgroundColor:'green',
+    flex:1,
+    alignItems:'center',
+    marginTop:hp(2)
+},
+subcategory_list_touchable_opacity:{
+    borderWidth:2,
+    width:hp(18),
+    height:hp(24),
+    borderColor:'#2f2e7e',
+    margin:hp(1.5),
+    backgroundColor:'#e8fcfc',
+    borderRadius:7
 
-    nocategorycontainer:{
-        backgroundColor:'#ffffff',
-        marginTop:5,
-        padding:10,
-        height:700
-    },
-    nocategorylist:{
-        backgroundColor:'yellow',
-        margin:5,
-        flexDirection:'row',
-        columnGap:20,
-        backgroundColor:'#f2f2f7',
-        padding:10,
-        alignItems:'center',
-        borderRadius:10
-    },
-    image_container:{
-        width:65,
-        height:65,
-        borderRadius:5
-       },
-       list:{
-        margin:5,
-        flexDirection:'row',
-        columnGap:20,
-        backgroundColor:'#f2f2f7',
-        padding:10,
-        alignItems:'center',
-        borderRadius:10
-     
-       },
-       image_container:{
-        width:65,
-        height:65,
-       //  backgroundColor:'lightgreen',
-        borderRadius:5
-       },
-       name_price_container:{
-         justifyContent:'center',
-       },
-       item_name:{
-        width:220,
-        height:40,
-        overflow:'hidden',
-        textAlign:"left",
-       //  backgroundColor:'white',
-        fontSize:16,
-        color:'#2f2e7e',
-        fontWeight:'bold'
-       
-       },
-       item_price:{
-       paddingTop:5,
-       fontSize:16,
-       fontWeight:'bold',
-       color:'red',
-       marginLeft:10
-       },
+},
+subcategory_list:{
+    
+},
+subcategory_image_container:{
+    width:"100%",
+    height:hp(15),
+    padding:hp(1.1) ,
+    backgroundColor:'#ffffff',
+    borderRadius:5
+},
+subcategory_name_container:{
+    // backgroundColor:'yellow',
+    width:'100%',
+    height:hp(8),
+    alignItems:'center',
+    justifyContent:'center'
+},
+subcategory_name:{
+    textAlign:'center',
+    color:'#2f2e7e',
+    fontWeight:'500',
+    padding:5
+},
+
 })
+
 export default SubCategory
