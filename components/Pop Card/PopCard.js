06f910cde75587f9a80fb659dbc25839c2f45160
vigneshdharuman01 <vigneshdharuman01@gmail.com>
2024-01-29 15:42:53 +0530

    responsive design compilited in orders and productdetail page and change text height in category and subcategory page

--- components/Pop Card/PopCard.js
+++ components/Pop Card/PopCard.js
@@ -370,8 +370,7 @@ const styles = StyleSheet.create({
         },
         content_container:{
             backgroundColor:'#e8fcfc',
-            borderBottomRightRadius:20,
-            borderBottomLeftRadius:20
+            padding:10
         }
     
     
